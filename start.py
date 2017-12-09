76ead2495c46a1408f73fdfe4720fabf81bab2a3
Lucas <lucas.fijen@gmail.com>
2017-12-09 14:27:20 +0100

    Test database

--- start.py
+++ start.py
@@ -1,4 +1,8 @@
 import numpy as np
 
-data = np.loadtxt('data')
-print(data)
+directors = np.loadtxt('database/directors.csv', dtype='str', delimiter=',')
+composers = np.loadtxt('database/composers.csv', dtype='str', delimiter=',')
+actors = np.loadtxt('database/actors.csv', dtype='str', delimiter=',')
+business = np.loadtxt('database/business.csv', dtype='str', delimiter=',')
+special = np.loadtxt('database/special-effects-companies.csv', dtype='str', delimiter=',')
+print(directors)
